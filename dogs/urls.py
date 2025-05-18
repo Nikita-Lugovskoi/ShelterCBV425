6ca39679f363cac4dd6c2930d72a87cfacfea679
Nikita-Lugovskoi <maloooy11@gmail.com>
2025-05-18 19:14:54 +1000

    1. Переписали dogs_list_view на DogListView 2. Внесли изменения в dogs.urls 3. Скорректированы шаблоны

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from dogs.apps import DogsConfig
 
-from dogs.views import index, breeds_list_view, breed_dogs_list_view, dogs_list_view, dog_create_view, dog_detail_view, dog_update_view, dog_delete_view
+from dogs.views import index, breeds_list_view, breed_dogs_list_view, DogListView, dog_create_view, dog_detail_view, dog_update_view, dog_delete_view
 
 app_name = DogsConfig.name
 
@@ -12,7 +12,7 @@ urlpatterns = [
     
     #dogs
     path('breeds/<int:pk>/dogs/', breed_dogs_list_view, name='breed_dogs'),
-    path('dogs/', dogs_list_view, name='dogs_list'),
+    path('dogs/', DogListView.as_view(), name='dogs_list'),
     path('dogs/create/', dog_create_view, name='dog_create'),
     path('dogs/detail/<int:pk>/', dog_detail_view, name='dog_detail'),
     path('dogs/update/<int:pk>', dog_update_view, name='dog_update'),
