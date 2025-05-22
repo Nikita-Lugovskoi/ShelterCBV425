f1b42afa690ae9ee4f94ad8e368e04949d1611be
Nikita-Lugovskoi <maloooy11@gmail.com>
2025-05-22 13:50:54 +1000

    1.Добавлен dog_toggle_activity (возможность изменять автивность карточки). 2.добавлены нужные urls. 3. внесены изменения в Dog From

--- dogs/urls.py
+++ dogs/urls.py
@@ -3,7 +3,7 @@ from dogs.apps import DogsConfig
 from django.views.decorators.cache import cache_page, never_cache
 
 from dogs.views import index, BreedsListView, DogBreedListVeiw, DogListView, DogCreateView, DogDetailView, DogUpdateView, \
-                       DogDeleteView
+                       DogDeleteView, DogDeactivetedListView, dog_toggle_activity
 
 app_name = DogsConfig.name
 
@@ -15,8 +15,10 @@ urlpatterns = [
     #dogs
     path('breeds/<int:pk>/dogs/', DogBreedListVeiw.as_view(), name='breed_dogs'),
     path('dogs/', DogListView.as_view(), name='dogs_list'),
+    path('dogs/', DogDeactivetedListView.as_view(), name='dogs_deactiveted_list'),
     path('dogs/create/', DogCreateView.as_view(), name='dog_create'),
     path('dogs/detail/<int:pk>/', DogDetailView.as_view(), name='dog_detail'),
     path('dogs/update/<int:pk>', never_cache(DogUpdateView.as_view()), name='dog_update'),
+    path('dogs/update/<int:pk>', dog_toggle_activity, name='dog_toggle_activity'),
     path('dogs/delete/<int:pk>', DogDeleteView.as_view(), name='dog_delete'),
 ]
