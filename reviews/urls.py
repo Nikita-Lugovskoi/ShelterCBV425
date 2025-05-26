2f60ad208786246b2b72efcc74aeea1cc8861b9c
Nikita-Lugovskoi <maloooy11@gmail.com>
2025-05-26 15:48:46 +1000

    1.Добавлен CRUD для Reviews. 2.Прописаны urls. 3. Добавлены пустые шаблоны

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 
 from reviews.apps import ReviewsConfig
 
-from reviews.views import ReviewListView, ReviewDeactivatedListView
+from reviews.views import ReviewListView, ReviewDeactivatedListView, ReviewCreateView, ReviewUpdateView, \
+                          ReviewDetailView, ReviewDeleteView 
 
 app_name = ReviewsConfig.name
 
 urlpatterns = [
     path('', ReviewListView.as_view(), name='reviews_list'),
     path('deactivated/', ReviewDeactivatedListView.as_view(), name='reviews_deactivated_list'),
+    path('create/', ReviewCreateView.as_view(), name='review_create'),
+    path('detail/', ReviewDetailView.as_view(), name='review_detail'),
+    path('update/', ReviewUpdateView.as_view(), name='review_update'),
+    path('delete/', ReviewDeleteView.as_view(), name='review_delete'),
 ]
